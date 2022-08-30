7d266e10afb5d83a9c1f0c24e1f6a7677d0f1206
Matheus Oliveira <matheus_luiz2011@hotmail.com>
2022-08-30 03:29:28 -0300

    Category

---
+++ src/main/java/com/example/WebServicesProject/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.example.WebServicesProject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.WebServicesProject.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
+        
+
