c9f8bc80182366e8c45cf7ab5b4acc719d88440e
Matheus Oliveira <matheus_luiz2011@hotmail.com>
2022-08-30 20:08:05 -0300

    Payment, one-to-one association

--- src/main/java/com/example/WebServicesProject/config/TestConfig.java
+++ src/main/java/com/example/WebServicesProject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.WebServicesProject.entities.Category;
 import com.example.WebServicesProject.entities.Order;
 import com.example.WebServicesProject.entities.OrderItem;
+import com.example.WebServicesProject.entities.Payment;
 import com.example.WebServicesProject.entities.Product;
 import com.example.WebServicesProject.entities.User;
 import com.example.WebServicesProject.entities.enums.OrderStatus;
@@ -23,6 +24,8 @@ import com.example.WebServicesProject.repositories.UserRepository;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
+
+    private static final String Payment = null;
 
     @Autowired
     private UserRepository userRepository;
@@ -78,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
--- src/main/java/com/example/WebServicesProject/entities/Order.java
+++ src/main/java/com/example/WebServicesProject/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.WebServicesProject.entities.enums.OrderStatus;
@@ -36,6 +38,9 @@ public class Order implements Serializable{
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) // mapeado o Id para se ter o mesmo valor
+    private Payment payment;
 
     public Order(){}
     public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
@@ -69,6 +74,13 @@ public class Order implements Serializable{
         if(orderStatus != null){
         this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
     public Set<OrderItem> getItems(){
         return items;
